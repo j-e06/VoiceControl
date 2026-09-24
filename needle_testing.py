5b0ff5f9e97c7d596135996cb5501ab2ee658667
je <je2006@duck.com>
2026-09-24 17:49:34 +0300

    Testing led blinking and telling user something

--- needle_testing.py
+++ needle_testing.py
@@ -1,15 +1,29 @@
 from time import sleep
-import needle_testing
+import needle
 from gpiozero import LED
-from pathlib import Path
+from poiper import play_sound
 
-@needle_testing.tool
+@needle.tool
 def blink_led(pin: int):
+    """Turn an led on for one second, and off."""
     led = LED(pin)
     led.on()
     sleep(1)
     led.off()
     sleep(1)
+    return f"LED on pin {pin} blinked."
 
-agent = needle_testing.Needle(tools=[blink_led])
-print(agent.run("Blink the led at pin 14.")["results"])
+@needle.tool
+def talk_to_user(instruction:str):
+    """Talk to the user."""
+    print(instruction)
+    play_sound(instruction)
+    return f"User yapped to."
+
+
+agent = needle.Needle(tools=[blink_led, talk_to_user])
+result = agent.run(
+    "Blink the led at pin 14 and say hello to the user"
+)
+
+print(result)
--- poiper.py
+++ poiper.py
@@ -1,11 +1,18 @@
 import wave
 from piper import PiperVoice, SynthesisConfig
-syn_config = SynthesisConfig(
-        volume = 3,
-        noise_scale = 1.0,
-        noise_w_scale=1.0
-        )
+import subprocess
 
 voice = PiperVoice.load("en_US-arctic-medium.onnx")
-with wave.open("test.wav", "wb") as wav_file:
-    voice.synthesize_wav("you need help you short person", wav_file, syn_config=syn_config)
+
+syn_config = SynthesisConfig(
+    volume=3,
+    noise_scale=1.0,
+    noise_w_scale=1.0
+)
+
+def play_sound(instruction: str):
+    with wave.open("test.wav", "wb") as wav_file:
+        voice.synthesize_wav(instruction, wav_file, syn_config=syn_config)
+    subprocess.call(["aplay", "test.wav"], check=True)
+    #subprocess.Popen(["rm", "test.wav"]) # remove file after playing.
+    print("Done.")
